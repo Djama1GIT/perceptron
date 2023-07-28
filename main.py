37a0e998e5ca7fb9b8b50b948cb32c67e2a06580
Djama1GIT <mail@dj.ama1.ru>
2023-07-28 16:31:53 +0300

    MLP

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import sys
 
 import numpy as np
@@ -7,76 +6,157 @@ sys.setrecursionlimit(2000000)
 
 
 def foo(a, b, c, d, e, f):
-    return a and b and c or d or (not e) and f or a and f
+    return a and b and c or d and e or f
 
 
 class Perceptron:
     def __init__(self, _inputs, _outputs):
         self.inputs = _inputs
         self.outputs = _outputs
-        self.synapses = 2 * np.random.random((6, 1)) - 1
+        self.synapses = np.random.randn(_inputs.shape[1], _outputs.shape[1])
 
     def __call__(self, *args, **kwargs):
         self.backpropagation()
         return self.synapses
 
     def backpropagation(self):
-        for i in range(10000):
-            __input = self.inputs
-            __outputs = Perceptron.sigmoid(np.dot(__input, self.synapses))
+        for i in range(1000000):
+            __inputs = self.inputs
+            __outputs = self.ReLU(np.dot(__inputs, self.synapses))
 
             err = self.outputs - __outputs
-            adjustments = np.dot(__input.T, err * (__outputs * (1 - __outputs)))
+            adjustments = np.dot(__inputs.T, err * self.ReLU_derivative(__outputs))
 
             self.synapses += adjustments
 
     @staticmethod
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
+    def ReLU(x: np.array) -> np.array:
+        return np.maximum(x, 0)
+
+    @staticmethod
+    def ReLU_derivative(x: np.array) -> np.array:
+        return np.where(x > 0, 1, 0)
 
 
-test_count = 0
+class MLP:
+    def __init__(self, inputs, outputs, hidden_units=None):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.hidden_units = hidden_units or [10, 10]
+        self.synapses = []
+        for i in range(len(self.hidden_units) + 1):
+            if i == 0:
+                synapse = np.random.randn(inputs.shape[1], self.hidden_units[i])
+            elif i == len(self.hidden_units):
+                synapse = np.random.randn(self.hidden_units[-1], outputs.shape[1])
+            else:
+                synapse = np.random.randn(self.hidden_units[i - 1], self.hidden_units[i])
+            self.synapses.append(synapse)
+
+    def __call__(self, *args, **kwargs):
+        self.backpropagation()
+        return self.synapses
+
+    def backpropagation(self):
+        for i in range(1000):
+            inputs = self.inputs
+            layers = [inputs]
+            for j in range(len(self.hidden_units)):
+                layer = np.tanh(np.dot(layers[-1], self.synapses[j]))
+                layers.append(layer)
+            outputs = np.tanh(np.dot(layers[-1], self.synapses[-1]))
+
+            output_error = self.outputs - outputs
+            output_adjustments = np.dot(layers[-1].T, output_error * (1 - np.square(outputs)))
+
+            hidden_errors = [output_error]
+            hidden_adjustments = []
+            for j in range(len(self.hidden_units), 0, -1):
+                error = np.dot(hidden_errors[-1], self.synapses[j].T)
+                hidden_errors.append(error)
+                adjustment = np.dot(layers[j - 1].T, error * (1 - np.square(layers[j])))
+                hidden_adjustments.append(adjustment)
+            hidden_adjustments.reverse()
+
+            self.synapses[-1] += output_adjustments
+            for j in range(len(self.hidden_units)):
+                self.synapses[j] += hidden_adjustments[j]
+
+
+counter = 0
+mlp: MLP = None
 
 
 def test_perceptron():
-    try:
-        global test_count
-        test_count += 1
-        inputs = []
-        for a in [0, 1]:
-            for b in [0, 1]:
-                for c in [0, 1]:
-                    for d in [0, 1]:
-                        for e in [0, 1]:
-                            for f in [0, 1]:
-                                if random.randint(0, 1):
-                                    inputs += [[a, b, c, d, e, f]]
-                                    # Интересный факт: если в инпут поместить все данные,
-                                    # то выйдет НИЧЕГО
+    global mlp
+    inputs = np.array([
+        [0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0, 1],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 0, 1],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 1, 0],
+        [1, 1, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 0],
+        [1, 0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 0, 1],
+        [0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 1, 1, 0, 0, 1],
+        [1, 1, 1, 1, 0, 0],
+        [1, 1, 1, 0, 1, 0],
+        [1, 1, 1, 0, 0, 1],
+    ])
 
-        inputs = np.array(inputs)
-        outputs = np.array([[foo(*i) for i in inputs]]).T
-        perceptron = Perceptron(inputs, outputs)
-        perceptron()
-        for a in [0, 1]:
-            for b in [0, 1]:
-                for c in [0, 1]:
-                    for d in [0, 1]:
-                        for e in [0, 1]:
-                            for f in [0, 1]:
-                                print((a, b, c, d, e, f), foo(a, b, c, d, e, f),
-                                      Perceptron.sigmoid(np.dot(
-                                          np.array([a, b, c, d, e, f]), perceptron.synapses)
-                                      )[0])
-                                assert (foo(a, b, c, d, e, f) == 1) == (round(
-                                    Perceptron.sigmoid(np.dot(
-                                        np.array([a, b, c, d, e, f]), perceptron.synapses)
-                                    )[0]) > 0.5) or print(f"Тест #{test_count} Не прошел")
-        else:
-            print(inputs)
-            print(f"Тест #{test_count} пройден. Перцептрон работает.")  # я пытался, не вышло
-    except:
-        test_perceptron()
+    outputs = np.array([[foo(*i)] for i in inputs])
+
+    mlp = MLP(inputs, outputs, hidden_units=[2, 3])
+    synapses = mlp()
+
+    new_inputs = np.array([
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [1, 0, 0, 1, 1, 0],
+        [1, 0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 0],
+    ])
+
+    expected_outputs = np.array([[foo(*i)] for i in new_inputs])
+    global counter
+    for i in range(len(new_inputs)):
+        result = np.tanh(np.dot(np.tanh(np.dot(new_inputs[i], synapses[0])), synapses[1]))
+        result = np.round(result, decimals=2)
+        print(f"Input: {new_inputs[i]} Output: {result[0]}, Expected: {expected_outputs[i][0]}")
+        if (result[0] > 0) != bool(expected_outputs[i][0]):
+            counter += 1
+            print(f"Тест #{counter} не пройден.")
+            test_perceptron()
+            break
 
 
 test_perceptron()
+print(f"Тест #{counter} пройден.")
+print(mlp.synapses)
